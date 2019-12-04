5c655639dd4b65f795372ce449170881039224a0
Darrell Whittall <darrellwhittall@gmail.com>
2019-12-04 10:18:00 +0000

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Day1
+{
+    internal class Program
+    {
+        public static void Main( string[] args )
+        {
+            var list = File.ReadAllLines("input1.txt");
+            foreach (var l in list)
+            {
+                Console.WriteLine(l);  
+            }
+            
+        }
+    }
+}
---
+++ Utils/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Utils
+{
+    public static class File
+    {
+        
+    }
+}
