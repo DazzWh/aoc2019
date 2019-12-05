a7f17e07ed9a946ca285c401ba45cd80f170d213
Darrell Whittall <darrellwhittall@gmail.com>
2019-12-05 10:44:43 +0000

    Abstracted intcode computer to util project

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -1,51 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using Utils;
 
 namespace Day2
 {
-    class Day2
+    internal class Day2
     {
-        static void Main( string[] args )
+        private static void Main( string[] args )
         {
             var input = File.ReadLines("input.txt").Single().Split(",").Select(int.Parse).ToList();
 
-            // Restore stuffs
             input[1] = 12;
             input[2] = 2;
-            
-            for (var i = 0; i < input.Count - 3; i += 4)
-            {
-                switch (input[i])
-                {
-                    case 99:
-                        Console.WriteLine(input[0]);
-                        return;
-                    case 1:
-                        // Add and put in 3rd
-                        input[input[i + 3]] = input[input[i + 1]] + input[input[i + 2]];
-                        break;
-                    case 2:
-                        // Multiply and put in 3rd
-                        input[input[i + 3]] = input[input[i + 1]] * input[input[i + 2]];
-                        break;
-                    default:
-                        throw new ArgumentException($"{input[i]} is an invalid operation");
-                }
-            }
-
-            
+            Console.Write(Intcode.Compute(input)[0]);
         }
 
-        /*
-         * OpCode
-         * Get input
-         * 99 = End
-         * 1 = Adds next two numbers and stores it in the 3rd position
-         * 2 = Same as above but multiply
-         *
-         * Move iterator 4 forward.
-         * Goto get input.
-         */
+        
     }
 }
---
+++ Utils/Intcode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    public static class Intcode
+    {
+
+        /*
+         OpCode, beginning of an instruction.
+         
+        OpCodes:
+        99 = End program
+
+        1  = Add
+           array[1, x, y, z]
+           array[z] = array[x] + array[y]
+
+        2  = Multiply
+           array[1, x, y, z]
+           array[z] = array[x] * array[y]
+        */
+
+        public static List<int> Compute(List<int> input)
+        {
+            for (var i = 0; i < input.Count; i += 4)
+            {
+                switch (input[i])
+                {
+                    case 99: // End program
+                        return input;
+
+                    case 1: // Add
+                        input[input[i + 3]] = input[input[i + 1]] + input[input[i + 2]];
+                        break;
+
+                    case 2: // Multiply
+                        input[input[i + 3]] = input[input[i + 1]] * input[input[i + 2]];
+                        break;
+
+                    default:
+                        throw new ArgumentException($"{input[i]} is an invalid operation");
+                }
+            }
+
+            return input;
+        }
+    }
+}
