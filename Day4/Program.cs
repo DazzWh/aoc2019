498e8114898c5e09545644bf1e03053364c26eba
Darrell Whittall <darrellwhittall@gmail.com>
2019-12-06 11:42:36 +0000

    Day4 Part2...

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -3,23 +3,62 @@ using System.Linq;
 
 namespace Day4
 {
-    class Program
+    internal class Program
     {
-        static void Main( string[] args )
+        internal static void Main( string[] args )
         {
 
             var total = 0;
 
-            for (int i = 272091; i < 815432; i++)
+            for (var i = 272091; i < 815432; i++)
             {
                 var str = i.ToString();
 
                 // left to right, digits never decease
                 // at least 2 adjacent digits are the same
-                if (str.Zip(str.Skip(1), (a, b) => (a <= b)).All(x => x) &&
-                    str.Zip(str.Skip(1), (a, b) => (a == b)).Any(x => x))
-                    total++;
+                if (!str.Zip(str.Skip(1), (a, b) => (a <= b)).All(x => x) ||
+                    !str.Zip(str.Skip(1), (a, b) => (a == b)).Any(x => x))
+                    continue;
 
+                // Sequential numbers aren't grouped larger than their number
+                // [3,3] fine
+                // [3, 3, 3, 3] not fine
+                // ABOVE IS WRONG
+
+                // 
+
+                // It's friday morning after an xmas party, I'm going to write this horribly.
+                var num = -1; // Current number
+                var cons = 0; // Amount of current numbers that have been consecutively placed so far
+                var fine = false;
+                foreach (var c in str)
+                {
+                    var value = int.Parse(c.ToString());
+
+                    if (value == num)
+                    {
+                        cons++;
+                    }
+                    else
+                    {
+                        if (cons == 1)
+                            fine = true;
+
+                        cons = 0;
+                        num = value;
+                    }
+                }
+
+                if (cons == 1)
+                    fine = true;
+
+                // If above is fine, increment total
+                if (fine)
+                {
+                    total++;
+                    Console.WriteLine(str);
+                }
+                
             }
 
             Console.WriteLine(total);
