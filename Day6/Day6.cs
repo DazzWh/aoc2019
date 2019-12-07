fa7ec5afef442ebf5e20ad9d8d1b30289e4f61e1
Darrell Whittall <darrellwhittall@gmail.com>
2019-12-07 18:10:08 +0000

    Rushed day 6 part 2

--- Day6/Day6.cs
+++ Day6/Day6.cs
@@ -9,6 +9,11 @@ namespace Day6
     {
         static void Main(string[] args)
         {
+
+            // It's saturday.
+            // I just want to get the answer and go to the pub
+            // Hence this mess. Worked first time though if that counts for anything.
+
             var input = File.ReadAllLines("input.txt").ToList();
 
             var planets = new HashSet<Planet>();
@@ -34,6 +39,55 @@ namespace Day6
                 }
             }
 
+            var san = planets.FirstOrDefault(p => p.Name == "SAN");
+            var you = planets.FirstOrDefault(p => p.Name == "YOU");
+
+            // Find closest common orbit to santa
+
+            // Get a list of planets you orbit
+            var pp = you.Orbits;
+            var youPlanets = new List<Planet>();
+            
+            while (pp != null)
+            {
+                youPlanets.Add(pp);
+                pp = pp.Orbits;
+            }
+
+            // Go down through santas planets until you find one in your list
+            // That is the closest common orbit to santa
+            
+            pp = san.Orbits;
+            var closestCommonPlanet = san.Orbits;
+            while (pp != null)
+            {
+                if (youPlanets.Contains(pp))
+                {
+                    closestCommonPlanet = pp;
+                    break;
+                }
+                pp = pp.Orbits;
+            }
+
+            // Count from santa to common planet, and you to common planet, add together and output
+            var total = 0;
+            pp = san.Orbits;
+            while (!pp.Equals(closestCommonPlanet))
+            {
+                total++;
+                pp = pp.Orbits;
+            }
+
+            pp = you.Orbits;
+            while (!pp.Equals(closestCommonPlanet))
+            {
+                total++;
+                pp = pp.Orbits;
+            }
+
+            Console.WriteLine(total);
+
+            /* Part 1
             var orbits = 0;
             // Then go through each planet,
             // counting the amount of planets it takes to get to a planet that has no orbit
@@ -49,6 +103,7 @@ namespace Day6
 
             // Output the total counts
             Console.WriteLine(orbits);
+            */
         }
 
     }
