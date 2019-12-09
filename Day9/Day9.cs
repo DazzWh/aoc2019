5e8d95fbe72c2366301ec663908302692d8b944f
Darrell Whittall <darrellwhittall@gmail.com>
2019-12-09 16:10:34 +0000

    Day9Part2

--- Day9/Day9.cs
+++ Day9/Day9.cs
@@ -7,10 +7,13 @@ namespace Day9
         private static void Main( string[] args )
         {
             var cpu = new Computer("input.txt");
-            cpu.AddInputs(new long[]{ 1 });
+            cpu.AddInputs(new long[]{ 2 });
             cpu.Run();
 
             // Part 1: 4234906522
+            // Part 2: 60962
+
+
         }
     }
 }
