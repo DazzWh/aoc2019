ed04fa2340f8823e0b625529b6b49424806c179c
Darrell Whittall <darrellwhittall@gmail.com>
2019-12-11 16:18:50 +0000

    Still stuck

--- Day11/Day11.cs
+++ Day11/Day11.cs
@@ -14,8 +14,6 @@ namespace Day11
     {
         internal static void Main( string[] args )
         {
-            // 4563 is too high, not 4562
-
             var cpu = new Computer("input.txt")
             {
                 OutputToConsole = false,
@@ -25,32 +23,60 @@ namespace Day11
             var robo = new Robot();
             var board = new Dictionary<Point, long>();
 
+            // First input
             cpu.AddInputs(new long[] { 0 });
-            cpu.Run();
 
             while (true)
             {
+                cpu.Run();
+                if (cpu.OutputLog.Single().Equals("Complete"))
+                    break;
+
                 // First output is what to paint
                 var p = new Point(robo.X, robo.Y);
-                board[p] = long.Parse(cpu.OutputLog.Single());
-
-                cpu.Run();
+                board[p] = int.Parse(cpu.OutputLog.Single());
+                
                 // Second is where to turn
-                robo.Turn(cpu.OutputLog.Last());
+                cpu.Run();
+                robo.Turn(int.Parse(cpu.OutputLog.Single()));
 
                 // Input what the position under the robot is
                 p = new Point(robo.X, robo.Y);
-                board.TryGetValue(p, out var v);
-                cpu.AddInputs(new[] { v });
-
-                cpu.Run();
-                if (cpu.OutputLog.Last().Equals("Complete"))
-                    break;
+                cpu.AddInputs(board.ContainsKey(p) ? new[] { board[p] } : new long[] {0});
             }
 
-            // board.Keys.ToList().ForEach(Console.WriteLine);
-
+            // Print the points hit
+            board.Keys.ToList().ForEach(Console.WriteLine);
             Console.Out.WriteLine($"Count: { board.Count }");
+
+            // Print the board
+            PrintBoard(board);
+        }
+
+        private static void PrintBoard(Dictionary<Point, long> board)
+        {
+            var minX = board.Min(x => x.Key.X);
+            var minY = board.Min(y => y.Key.Y);
+            var maxX = board.Max(x => x.Key.X);
+            var maxY = board.Max(y => y.Key.Y);
+
+            for (var y = minY; y < maxY ; y++)
+            {
+                var line = "";
+                for (var x = minX; x < maxX; x++)
+                {
+                    if (board.ContainsKey(new Point(x, y)) && board[new Point(x, y)].Equals(1))
+                    {
+                        line += board[new Point(x, y)];
+                    }
+                    else
+                    {
+                        line += " ";
+                    }
+                }
+                Console.WriteLine(line);
+            }
+
         }
     }
 
@@ -61,7 +87,7 @@ namespace Day11
 
     internal class Robot
     {
-        public Direction dir;
+        private Direction dir;
         public int X;
         public int Y;
         
@@ -72,33 +98,29 @@ namespace Day11
             X = Y = 0;
         }
         
-        public void Turn(string d)
+        public void Turn(int d)
         {
-            if (d.Equals("0"))
-            {
-                dir += -1;
-            }
-            else
+            if (d == 0)
             {
                 dir += 1;
             }
+            else
+            {
+                dir -= 1;
+            }
 
-            if ((int) dir == -1)
-                dir = Direction.Down;
+            if (dir < Direction.Left) dir = Direction.Down;
+            if (dir > Direction.Down) dir = Direction.Left; 
 
-            if ((int) dir == 4)
-                dir = Direction.Left;
-
-            // Move forward 1 space
             switch (dir)
             {
-                case Direction.Up:    Y += 1; break;
-                case Direction.Down:  Y -= 1; break;
-                case Direction.Left:  X -= 1; break;
+                case Direction.Left: X -= 1; break;
+                case Direction.Up: Y += 1; break;
                 case Direction.Right: X += 1; break;
+                case Direction.Down: Y -= 1; break;
 
                 default:
-                    throw new ArgumentException("Invalid Direction");
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
--- Day9/Day9.cs
+++ Day9/Day9.cs
@@ -7,7 +7,7 @@ namespace Day9
         private static void Main( string[] args )
         {
             var cpu = new Computer("input.txt");
-            cpu.AddInputs(new long[]{ 2 });
+            cpu.AddInputs(new long[]{ 1 });
             cpu.Run();
 
             // Part 1: 4234906522
